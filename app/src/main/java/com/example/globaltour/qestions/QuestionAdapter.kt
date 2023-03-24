f8b026ad41f3281e390ed4c11f651c649fe2984f
Alice <amusukwa@gmail.com>
2023-03-24 09:35:39 +0200

    editing questions adapter

--- app/src/main/java/com/example/globaltour/qestions/QuestionAdapter.kt
+++ app/src/main/java/com/example/globaltour/qestions/QuestionAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.globaltour.qestions
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -18,21 +19,34 @@ class QuestionAdapter(val context: Context, var QuestionList: ArrayList<Question
 //        private var currentPosition: Int = -1
        private lateinit var question: Question
 
-        private val questiontxt = itemView.findViewById<TextView>(R.id.questionName)
+        private val questiontxt = itemView.findViewById<TextView>(R.id.questionName_txt)
         private val usernametxt= itemView.findViewById<TextView>(R.id.username_text)
+
+        fun bind(question: Question, position: Int) {
+
+            questiontxt.text = question.questionName
+            usernametxt.text = question.username
+
+
+        }
 
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
-        TODO("Not yet implemented")
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.question_item, parent, false)
+        return QuestionViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val question = QuestionList[position]
+        holder.bind(question, position)
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return QuestionList.size
     }
 
 }
