697eae7476c8d46f3d5976df95c2eb9181fb1688
Alice <amusukwa@gmail.com>
2023-03-28 10:44:38 +0200

    Adding receyclerview to questionsList

--- app/src/main/java/com/example/globaltour/qestions/QuestionsList.kt
+++ app/src/main/java/com/example/globaltour/qestions/QuestionsList.kt
@@ -1,23 +1,28 @@
 package com.example.globaltour.qestions
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.globaltour.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 
 class QuestionsList : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var questionList: ArrayList<Question>
+    private lateinit var questionAdapter: QuestionAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,5 +36,35 @@ class QuestionsList : Fragment() {
 }
 
     private fun setUpRecyclerView(view: View?) {
+
+          var docRef = FirebaseDatabase.getInstance()
+          val context = requireContext()
+          questionList =  arrayListOf<Question>()
+        questionAdapter = QuestionAdapter(context, questionList)
+        val recyclerView =  view?.findViewById<RecyclerView>(R.id.questions_recycler)
+         recyclerView?.adapter = questionAdapter
+
+
+//        docRef.getReference("cities").child("globacities").
+//        addValueEventListener(object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                if(snapshot.exists()){
+//                    for(mentorSnapshot in snapshot.children){
+//                        val globalcity = snapshot.getValue(GlobalCity::class.java)
+//                        Log.d(ContentValues.TAG,"error getting document   ${globalcity}")
+//                        globalcitiesList.add(globalcity!!)
+//                    }
+//                    recyclerView?.adapter = NewCityAdapter(context,globalcitiesList)
+//                }
+//
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.d(ContentValues.TAG,"error getting document")
+//            }
+//        })
+//
+
+
 
     }}
