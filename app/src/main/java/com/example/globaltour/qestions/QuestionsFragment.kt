ca4ae7b6e7b8ae8ba99cf88acdfa7ddb473a41d1
Alice <amusukwa@gmail.com>
2023-03-29 20:16:53 +0200

    adding google map

--- app/src/main/java/com/example/globaltour/qestions/QuestionsFragment.kt
+++ app/src/main/java/com/example/globaltour/qestions/QuestionsFragment.kt
@@ -1,13 +1,16 @@
 package com.example.globaltour.qestions
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.view.MenuProvider
+import com.example.globaltour.MainActivity
+import com.example.globaltour.MapsActivity
 import com.example.globaltour.R
 import com.google.firebase.database.FirebaseDatabase
 
@@ -15,18 +18,19 @@ import com.google.firebase.database.FirebaseDatabase
 
 
 
-class QuestionsFragment : Fragment() {
+class QuestionsFragment : Fragment(),MenuProvider {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_questions, container, false)
 
-        return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity?.addMenuProvider(this)
         val addButton = view.findViewById<Button>(R.id.questionButton)
+        val mapButton = view.findViewById<Button>(R.id.button_map)
 
         addButton.setOnClickListener {
             val docRef = FirebaseDatabase.getInstance()
@@ -44,13 +48,32 @@ class QuestionsFragment : Fragment() {
                 .setValue(question)
 
             Toast.makeText(this@QuestionsFragment.requireActivity(),"saving",Toast.LENGTH_SHORT).show()
-
+        }
+        mapButton.setOnClickListener {
+            val intent = Intent (activity,MapsActivity::class.java)
+            startActivity(intent)
         }
 
+    }
+    //creating the options menu
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.map_menu,menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        if (menuItem.itemId == R.id.action_map) {
+            Toast.makeText(this@QuestionsFragment.requireActivity(), "saving", Toast.LENGTH_SHORT)
+                .show()
+            return true
+
+        }
+        return true
+
+    }
 
 
+}
 
-    }}
 
 
 
