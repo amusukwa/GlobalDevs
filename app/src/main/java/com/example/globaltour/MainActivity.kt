31738e464e4ecb8f6f7ddfb2290b884943805984
alice <amusukwa@gmail.com>
2024-03-24 19:19:56 +0200

    resolving auth bug

--- app/src/main/java/com/example/globaltour/MainActivity.kt
+++ app/src/main/java/com/example/globaltour/MainActivity.kt
@@ -16,23 +16,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toolbar        : Toolbar
     private lateinit var navController  : NavController
     private lateinit var bottomNavView  : BottomNavigationView
+    private lateinit var auth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize Views
         toolbar         = findViewById(R.id.activity_main_toolbar)
         bottomNavView   = findViewById(R.id.bottom_nav_view)
+        auth = FirebaseAuth.getInstance()
 
-        // Check if user is signed in
-//        val currentUser = FirebaseAuth.getInstance().currentUser
-//        if (currentUser == null) {
-//            // If user is not signed in, navigate to sign-in activity
-//            startActivity(Intent(this, SignInActivity::class.java))
-//            finish() // Finish MainActivity to prevent user from going back
-//            return
-//        }
 
         // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
@@ -48,4 +45,27 @@ class MainActivity : AppCompatActivity() {
         // Connect BottomNavigationView with NavController
         bottomNavView.setupWithNavController(navController)
     }
+
+    override fun onStart() {
+        super.onStart()
+        val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if (currentUser == null) {
+                // If user is not signed in, navigate to sign-in activity
+                startActivity(Intent(this, SignInActivity::class.java))
+                finish() // Finish MainActivity to prevent user from going back
+
+            }}
+        // Add authentication state listener when the activity starts
+        auth.addAuthStateListener(authStateListener)
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        // Remove authentication state listener when the activity stops
+        auth.removeAuthStateListener(authStateListener)
+    }
+
+
 }
