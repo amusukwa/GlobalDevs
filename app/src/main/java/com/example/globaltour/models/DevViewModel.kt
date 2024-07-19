a2be5bebf65ffe23fd825ea705e4247884937210
alice <amusukwa@gmail.com>
2024-07-19 10:22:50 +0200

    added viewmodel for devprofile

--- app/src/main/java/com/example/globaltour/models/DevViewModel.kt
+++ app/src/main/java/com/example/globaltour/models/DevViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.globaltour.models
 
-class DevViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.globaltour.Devprofile
+import com.example.globaltour.qestions.Question
+import com.example.globaltour.repository.DevProfileRepository
+import com.example.globaltour.repository.QuestionRepository
+
+class DevViewModel: ViewModel() {
+    private val repository: DevProfileRepository = DevProfileRepository().getInstance()
+    private val all_devs = MutableLiveData<List<Devprofile>>()
+    val All_devs : MutableLiveData<List<Devprofile>> = all_devs
+    //initialization
+    init {
+        repository.loadDevs(all_devs)
+
+    }
 }
